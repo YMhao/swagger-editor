4217cf87c80a71371feb50c8f44cd5d511338af4
yu <yu@759290728@qq.com>
2018-03-31 23:29:30 +0800

    yaml from url query

--- src/index.js
+++ src/index.js
@@ -15,6 +15,32 @@ import EditorAutosuggestOAS3KeywordsPlugin from "./plugins/editor-autosuggest-oa
 import EditorAutosuggestRefsPlugin from "./plugins/editor-autosuggest-refs"
 import PerformancePlugin from "./plugins/performance"
 import JumpToPathPlugin from "./plugins/jump-to-path"
+import CryptoJS from 'crypto-js'
+
+function getQueryString(name)
+{
+    var url = window.location.search
+    var reg = new RegExp('(^|&)'+ name +'=([^&]*)(&|$)','i')
+    var r = url.substr(1).match(reg)
+    if (r != null) return decodeURI(r[2]); return null
+}
+
+function base64_decode(dataBase64){
+	var words  = CryptoJS.enc.Base64.parse(dataBase64)
+	return words.toString(CryptoJS.enc.Utf8)
+}
+
+function getSwagger2GeneratorUrl(url) {
+  var swaggerProtocolUrlBase64 = getQueryString ('yamlUrl')
+  if(swaggerProtocolUrlBase64) {
+    var swaggerProtocolUrl = base64_decode(swaggerProtocolUrlBase64)
+    console.log(swaggerProtocolUrl)
+    if(swaggerProtocolUrl) {
+      return swaggerProtocolUrl
+    }
+  }
+  return url
+}
 
 // eslint-disable-next-line no-undef
 const { GIT_DIRTY, GIT_COMMIT, PACKAGE_VERSION } = buildInfo
@@ -47,7 +73,7 @@ const defaults = {
     EditorLayout
   },
   showExtensions: true,
-  swagger2GeneratorUrl: "https://generator.swagger.io/api/swagger.json",
+  swagger2GeneratorUrl: getSwagger2GeneratorUrl("https://generator.swagger.io/api/swagger.json"),
   oas3GeneratorUrl: "http://generator3.swagger.io/api/generator.json"
 }
 
--- src/plugins/local-storage/index.js
+++ src/plugins/local-storage/index.js
@@ -1,4 +1,7 @@
 import PetstoreYaml from "./petstore"
+import CryptoJS from 'crypto-js'
+import fetch from 'node-fetch';
+
 const CONTENT_KEY = "swagger-editor-content"
 
 let localStorage = window.localStorage
@@ -9,9 +12,40 @@ export const updateSpec = (ori) => (...args) => {
   saveContentToStorage(spec)
 }
 
+function getQueryString(name)
+{
+    var url = window.location.search
+    var reg = new RegExp('(^|&)'+ name +'=([^&]*)(&|$)','i')
+    var r = url.substr(1).match(reg)
+    if (r != null) return decodeURI(r[2]); return null
+}
+
+// function base64_encode(data){
+// 	var str=CryptoJS.enc.Utf8.parse(data)
+// 	var base64=CryptoJS.enc.Base64.stringify(str)
+// 	return base64
+// }
+function base64_decode(dataBase64){
+	var words  = CryptoJS.enc.Base64.parse(dataBase64)
+	return words.toString(CryptoJS.enc.Utf8)
+}
+
+function loadByQueryYamlUrl() {
+  var swaggerProtocolUrlBase64 = getQueryString ('yamlUrl')
+  if(swaggerProtocolUrlBase64) {
+    var swaggerProtocolUrl = base64_decode(swaggerProtocolUrlBase64)
+    fetch(swaggerProtocolUrl).then(res => res.text()).then(res => {
+      saveContentToStorage(res)
+      return res
+    })
+  }
+  return null
+}
+
 export default function(system) {
   // setTimeout runs on the next tick
   setTimeout(() => {
+    loadByQueryYamlUrl()
     if(localStorage.getItem(CONTENT_KEY)) {
       system.specActions.updateSpec(localStorage.getItem(CONTENT_KEY))
     } else if(localStorage.getItem("ngStorage-SwaggerEditorCache")) {
