34f80a1349d5d9b20cb45a62ba5858d671ececd7
yu <yu@759290728@qq.com>
2018-04-01 17:31:52 +0800

    load yaml from query

--- src/plugins/local-storage/index.js
+++ src/plugins/local-storage/index.js
@@ -1,5 +1,9 @@
 import PetstoreYaml from "./petstore"
+import CryptoJS from 'crypto-js'
+import "whatwg-fetch"
+
 const CONTENT_KEY = "swagger-editor-content"
+const CONTENT_YAML_URL_KEY = "swagger-yaml-url"
 
 let localStorage = window.localStorage
 
@@ -9,9 +13,55 @@ export const updateSpec = (ori) => (...args) => {
   saveContentToStorage(spec)
 }
 
+function getQueryString(name)
+{
+    var url = window.location.search
+    var reg = new RegExp('(^|&)'+ name +'=([^&]*)(&|$)','i')
+    var r = url.substr(1).match(reg)
+    if (r != null) return decodeURIComponent(r[2]); return null
+}
+
+function base64_decode(dataBase64){
+	var words  = CryptoJS.enc.Base64.parse(dataBase64)
+	return words.toString(CryptoJS.enc.Utf8)
+}
+
+function urlQueryDel(name){
+  var loca = window.location
+  var baseUrl = loca.origin + loca.pathname + "?"
+  var query = loca.search.substr(1)
+  if (query.indexOf(name)>-1) {
+      var obj = {}
+      var arr = query.split("&")
+      for (var i = 0; i < arr.length; i++) {
+          arr[i] = arr[i].split("=")
+          obj[arr[i][0]] = arr[i][1]
+      }
+      delete obj[name]
+      var url = baseUrl + JSON.stringify(obj).replace(/[\"\{\}]/g,"").replace(/\:/g,"=").replace(/\,/g,"&")
+      return url
+  }
+}
+
+function loadByQueryYamlUrl() {
+  var swaggerProtocolUrlBase64 = getQueryString ('yamlUrl')
+  if(swaggerProtocolUrlBase64) {
+    var swaggerProtocolUrl = base64_decode(swaggerProtocolUrlBase64)
+    fetch(swaggerProtocolUrl,{
+      method: 'GET',
+      mode: 'cors'
+    }).then(res => res.text()).then(res => {
+      saveContentToStorage(res)
+      window.location.href = urlQueryDel('yamlUrl')
+    })
+  }
+}
+
+
 export default function(system) {
   // setTimeout runs on the next tick
   setTimeout(() => {
+    loadByQueryYamlUrl()
     if(localStorage.getItem(CONTENT_KEY)) {
       system.specActions.updateSpec(localStorage.getItem(CONTENT_KEY))
     } else if(localStorage.getItem("ngStorage-SwaggerEditorCache")) {
